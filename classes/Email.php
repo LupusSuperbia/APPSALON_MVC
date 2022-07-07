663498f8d0adc5dcbb0e5781eea1e414a4c2c4a4
Pridewolf <agus_blackarrow_00@hotmail.com>
2022-07-06 21:59:45 -0300

    main

--- classes/Email.php
+++ classes/Email.php
@@ -32,7 +32,7 @@ class Email
         $mail->Password = '48d3655052b0fb';
 
         $mail->setFrom('cuentas@appsalon.com');
-        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $mail->addAddress($this->email, $this->nombre);
         $mail->Subject = "Confirma tu cuenta";
 
         // Set HTML
@@ -41,7 +41,7 @@ class Email
         $mail->CharSet = "UTF-8";
         $contenido = "<html>";
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has Creado tu cuenta en App Salon, solo debes confirmarla presionando el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='https://hidden-taiga-29998.herokuapp.com/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .= "</html>";
 
@@ -67,7 +67,7 @@ class Email
         $mail->Password = '48d3655052b0fb';
 
         $mail->setFrom('cuentas@appsalon.com');
-        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $mail->addAddress($this->email, $this->nombre);
         $mail->Subject = "Restablece tu password";
 
         // Set HTML
@@ -76,7 +76,7 @@ class Email
         $mail->CharSet = "UTF-8";
         $contenido = "<html>";
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado restablecer tu password sigue el suigiente enlace para hacerlo</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href=''https://hidden-taiga-29998.herokuapp.com/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .= "</html>";
 
